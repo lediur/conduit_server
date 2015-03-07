97dc2eb08a3b4453544395b9d12620178cfc1517
gnedivad <gnedivad@gmail.com>
2015-03-07 15:39:02 -0800

    Added routes related to user and car

--- README.md
+++ README.md
@@ -8,4 +8,11 @@ $ env/bin/pip install flask
 $ env/bin/pip install flask-sqlalchemy
 $ env/bin/pip install flask-wtf
 ```
-
+##Running the App in the Virtual Environment
+```
+$ env/bin/python run.py
+```
+##Sending Requests with cURL
+```
+$ curl -H "Content-Type: application/json" -d '@body.json' 'localhost:8080/user/create'
+```
--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # Import flask and template operators
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Import SQLAlchemy
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -17,7 +17,46 @@ db = SQLAlchemy(app)
 # Sample HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+  return render_template('404.html'), 404
+
+# User routes
+@app.route('/user', methods=['GET'])
+def user():
+  return 'GET /user\n'
+
+@app.route('/user/create', methods=['POST'])
+def user_create():
+  first = request.json['first']
+  last  = request.json['last']
+  phone = request.json['phone']
+  email = request.json['email']
+  # Create the user
+  return 'POST /user/create\n'
+
+@app.route('/user/update', methods=['POST'])
+def user_update():
+  return 'POST /user/update\n'
+
+@app.route('/user/delete', methods=['POST'])
+def user_delete():
+  return 'POST /user/delete\n'
+
+# Car routes
+@app.route('/car', methods=['GET'])
+def car():
+  return 'GET /car\n'
+
+@app.route('/car/create', methods=['POST'])
+def car_create():
+  return 'POST /car/create\n'
+
+@app.route('/car/update', methods=['POST'])
+def car_update():
+  return 'POST /car/update\n'
+
+@app.route('/car/delete', methods=['POST'])
+def car_delete():
+  return 'POST /car/delete\n'
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.mod_auth.controllers import mod_auth as auth_module
